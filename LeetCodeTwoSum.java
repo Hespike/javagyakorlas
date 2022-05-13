72a9a26af74b6db5a6dfabdc9624ad7ff86d5740
Sárdi András <sardi.andras@gmail.com>
2022-05-13 14:27:35 +0200

    Update LeetCodeTwoSum.java

--- LeetCodeTwoSum.java
+++ LeetCodeTwoSum.java
@@ -2,7 +2,7 @@
 You may assume that each input would have exactly one solution, and you may not use the same element twice. 
 You can return the answer in any order.*/
 
-class Megoldas {
+class Solution {
     public int[] twoSum(int[] nums, int target) {
         int finenums[];
         finenums = new int[2];
